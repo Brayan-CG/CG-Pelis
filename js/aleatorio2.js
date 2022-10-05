71d7c1c2c5b4c17e2b6a7a2d58b547d73fa5cee0
Brayan-CG <107977576+Brayan-CG@users.noreply.github.com>
2022-10-05 15:13:26 -0500

    Delete js directory

--- js/aleatorio2.js
+++
@@ -1,9 +0,0 @@
-function aleatorio() {
-    let enemy = Math.round(Math. random()*1)
-
-if (enemy == 0) {
-    location.href="Body/Búsqueda/24.html";
-}else  if (enemy == 1) {
-    location.href="Body/Búsqueda/36.html";
-}
-}
--- js/inicio.js
+++
@@ -1,8 +0,0 @@
-setTimeout(function() {
-    document.getElementById("mensajes").style.display="block";
-
-}, 1000*2);
-
-function cerrar(){
-    document.getElementById("mensajes").style.display="none"
-}
