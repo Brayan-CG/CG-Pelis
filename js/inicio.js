03a055765a261806d4815e58e67006f1848c3a02
Brayan-CG <107977576+Brayan-CG@users.noreply.github.com>
2022-10-05 15:12:02 -0500

    Add files via upload

--- js/inicio.js
+++ js/inicio.js
@@ -1,1 +1,8 @@
-H
+setTimeout(function() {
+    document.getElementById("mensajes").style.display="block";
+
+}, 1000*2);
+
+function cerrar(){
+    document.getElementById("mensajes").style.display="none"
+}
