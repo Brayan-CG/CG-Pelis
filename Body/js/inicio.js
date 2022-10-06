317feb3a3ac79d0dff1c7a77439436d768c1c3d8
Brayan-CG <107977576+Brayan-CG@users.noreply.github.com>
2022-10-06 14:59:07 -0500

    Add files via upload

--- Body/js/inicio.js
+++ Body/js/inicio.js
@@ -6,3 +6,31 @@ setTimeout(function() {
 function cerrar(){
     document.getElementById("mensajes").style.display="none"
 }
+
+function configuración(){
+    location.href="Body/Funciones/configuracion.html"
+}
+
+function series(){
+    location.href="Series.html"
+}
+
+function buscar(){
+      location.href="Body/Búsqueda/Búsqueda.html"
+}
+
+function buscar1(){
+      location.href="Body/Búsqueda/anime.html"
+}
+
+function buscar2(){
+      location.href="Body/Búsqueda/series.html"
+}
+
+function Anime(){
+    location.href="Anime.html"
+}
+
+function inicio(){
+    location.href="inicio.html"
+}
